93f04ea94cf31328bd9ecc4836dff3498f9c4db3
gihad <gihadmecha@gmail.com>
2020-09-12 16:02:33 +0200

    part_3

--- part_3/part_3.c
+++ part_3/part_3.c
@@ -10,8 +10,9 @@ typedef struct Player {
 int screenSetUp();
 int mapSetUp();
 Player* playerSetUp();
-int handleInput(int input, Player* hager);
-int playerMove(int y, int x, Player* hager);
+int handleInput(int input, Player* user);
+int checkPosition(int newY, int newX, Player* user);
+int playerMove(int y, int x, Player* user);
 
 int main()
 {
@@ -53,24 +54,24 @@ int mapSetUp(){
 	//(int) mvprintw (int,int, const char *,...) GCC_PRINTFLIKE(3,4);
 	//mvprintw(y, x, string);
 	mvprintw(13, 13,"--------");
-	mvprintw(14, 13,"|------|");
-	mvprintw(15, 13,"|------|");
-	mvprintw(16, 13,"|------|");
-	mvprintw(17, 13,"|------|");
+	mvprintw(14, 13,"|......|");
+	mvprintw(15, 13,"|......|");
+	mvprintw(16, 13,"|......|");
+	mvprintw(17, 13,"|......|");
 	mvprintw(18, 13,"--------");
 
 	mvprintw(2, 40,"--------");
-	mvprintw(3, 40,"|------|");
-	mvprintw(4, 40,"|------|");
-	mvprintw(5, 40,"|------|");
-	mvprintw(6, 40,"|------|");
+	mvprintw(3, 40,"|......|");
+	mvprintw(4, 40,"|......|");
+	mvprintw(5, 40,"|......|");
+	mvprintw(6, 40,"|......|");
 	mvprintw(7, 40,"--------");
 
 	mvprintw(10, 40,"------------");
-	mvprintw(11, 40,"|----------|");
-	mvprintw(12, 40,"|----------|");
-	mvprintw(13, 40,"|----------|");
-	mvprintw(14, 40,"|----------|");
+	mvprintw(11, 40,"|...........|");
+	mvprintw(12, 40,"|...........|");
+	mvprintw(13, 40,"|...........|");
+	mvprintw(14, 40,"|...........|");
 	mvprintw(15, 40,"------------");
 }
 
@@ -82,48 +83,69 @@ Player* playerSetUp(){
 	newPlayer->xPosition = 14;
 	newPlayer->health = 20;
 
-	mvprintw(newPlayer->yPosition, newPlayer->xPosition, "@");
 
-	//#define move(y,x)		wmove(stdscr,(y),(x))
-	move(newPlayer->yPosition,newPlayer->xPosition);
+	playerMove(newPlayer->yPosition, newPlayer->xPosition, newPlayer);
 
 	return newPlayer;
 }
 
-int handleInput(int input, Player* hager){
+int handleInput(int input, Player* user){
+	int newY;
+	int newX;
 	switch(input)
 	{	
 		//move up
 		case 'w':
 		case 'W':
-			playerMove(hager->yPosition - 1, hager->xPosition, hager);
+			newY = user->yPosition - 1;
+		    newX = user->xPosition;
 			break;
 		//move down
 		case 's':
 		case 'S':
-			playerMove(hager->yPosition + 1, hager->xPosition, hager);
+			newY = user->yPosition + 1;
+		    newX = user->xPosition;
 			break;
 	    //move left
 		case 'a':
 		case 'A':
-			playerMove(hager->yPosition, hager->xPosition - 1, hager);
+			newY = user->yPosition;
+		    newX = user->xPosition - 1;
 			break;
 		//move right
 		case 'd':
 		case 'D':
-			playerMove(hager->yPosition, hager->xPosition + 1, hager);
+			newY = user->yPosition;
+		    newX = user->xPosition + 1;
 			break;
 
 		default:
 			break;	
 	}
+
+	checkPosition(newY, newX, user);
+		
 }
 
-int playerMove(int y, int x, Player* hager){
+int checkPosition(int newY, int newX, Player* user){
+		int space;
+		//#define mvinch(y,x)			mvwinch(stdscr,(y),(x))
+		switch(mvinch(newY, newX))
+		{
+			case '.':
+				playerMove(newY, newX, user);
+				break;
+			default:
+				move(user->yPosition, user->xPosition);
+				break;
+		}
+}
 
-	mvprintw(hager->yPosition, hager->xPosition, ".");
-	hager->yPosition = y;
-	hager->xPosition = x;
-	mvprintw(hager->yPosition, hager->xPosition, "@");
-	move(hager->yPosition,hager->xPosition);
+int playerMove(int y, int x, Player* user){
+
+	mvprintw(user->yPosition, user->xPosition, ".");
+	user->yPosition = y;
+	user->xPosition = x;
+	mvprintw(user->yPosition, user->xPosition, "@");
+	move(user->yPosition, user->xPosition);
 }
